66a482725fd4877cd65f9423fa89384d3c15e2d0
MelikaE1380 <melikaeskandarzadeh9@gmail.com>
2025-03-25 02:11:25 -0600

    first commit on develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div>empty project and first change-div tag on master</div>;
+  return (
+    <div>
+      <div>empty project and first change-div tag on master</div>
+      <div>
+        this tag added after building a new branch from master named develop
+      </div>
+    </div>
+  );
 }
 
 export default App;
