a08da92a882006f66f48483819b248522b240ce1
MelikaE1380 <melikaeskandarzadeh9@gmail.com>
2025-03-25 02:33:15 -0600

    develop- description on merging develop and master

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ function App() {
       <div>
         this tag added after building a new branch from master named develop
       </div>
+      <div>
+        After first commit on develop"above tag" i want to apply develop changes
+        on master so what is the solution?? first i should commit this
+        change'exactly this sentence' on develop and then switch to master and
+        apply develop changes on master
+      </div>
     </div>
   );
 }
